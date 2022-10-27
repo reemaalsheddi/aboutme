4d5e74faae9e0cf8ba28c22f26e7905de5702819
hind <hind-almaaz@hotmail.com>
2022-10-27 14:57:45 +0300

    Initial Commit

---
+++ aboutme/aboutmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutmeApp.swift
+//  aboutme
+//
+//  Created by Hind Almaaz on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct aboutmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
