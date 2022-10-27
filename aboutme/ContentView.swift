8f299200d796b0758d6ce3e4f9bd367c088452c7
hind <hind-almaaz@hotmail.com>
2022-10-27 16:24:18 +0300

    h

--- aboutme/ContentView.swift
+++ aboutme/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color.indigo
+                .opacity(0.2)
+                .ignoresSafeArea()
+            VStack (alignment:.leading){
+                
+                Image("me")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 185)
+                            .stroke(.indigo, lineWidth: 12)
+                    )
+                    .padding(.bottom, 47.0)
+                
+                Text("About Me")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.indigo)
+                    .padding(/*@START_MENU_TOKEN@*/.bottom, 2.0/*@END_MENU_TOKEN@*/)
+                
+                Text("MY name is hind almaaz , content creator ,graphic designer wanna be app devaloper .")
+            }
+            .padding(.all)
+           
         }
-        .padding()
     }
 }
 
